2325aaac869e02dc6e22593c20a567a663b1e006
Priya1107-gif <priyadarshinisanker@gmail.com>
2025-05-18 23:42:36 +0530

    Delete week1/week3 directory

--- week1/week3/day1/PrintDuplicates.java
+++
@@ -1,7 +0,0 @@
-package week3.day1;
-
-public class PrintDuplicates {
-
-	int[] num={1, 2, 3};
-
-	}
--- week1/week3/day2/DatabaseConnection.java
+++
@@ -1,7 +0,0 @@
-package week3.day2;
-
-public interface DatabaseConnection {
-	void connect();
-	void disconnect();
-	void executeUpdate();
-}
